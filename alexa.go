8e4ac187c815be42260e11738058e2e7c6353095
Brian Glass <bglass@safaribooksonline.com>
2018-03-14 18:56:18 -0600

    Add preamble to launch intent

--- alexa.go
+++ alexa.go
@@ -41,17 +41,25 @@ func NewSkill(router *mux.Router, appid string, db *sql.DB, useJulian, doJump bo
 }
 
 func (self *Skill) launchHandler(request *alexa.EchoRequest, response *alexa.EchoResponse) {
+	var text, card string
+
 	today := time.Now().In(TZ)
 
 	factory := orthocal.NewDayFactory(self.useJulian, self.doJump, self.db)
 	day := factory.NewDay(today.Year(), int(today.Month()), today.Day(), self.bible)
+
+	text = "Today is the " + day.Titles[0] + ". "
+	text += FastingStatement(day) + " "
+	text += "The reading is from " + day.Readings[0].Display + ". "
+
+	card = text
 
 	var passage string
 	for _, verse := range day.Readings[0].Passage {
 		passage += verseRe.ReplaceAllString(verse.Content, "") + " "
 	}
 
-	response.OutputSpeech(passage).Card("Orthodox Daily", passage)
+	response.OutputSpeech(text+passage).Card("Orthodox Daily", card)
 }
 
 func (self *Skill) intentHandler(request *alexa.EchoRequest, response *alexa.EchoResponse) {
@@ -88,16 +96,16 @@ func FastingStatement(day *orthocal.Day) string {
 
 	hours := date.Sub(today).Hours()
 	if 0 <= hours && hours < 24 {
-		when = fmt.Sprintf("today, %s", date.Format("January 2"))
+		when = "Today, " + date.Format("January 2")
 	} else if 24 <= hours && hours < 48 {
-		when = fmt.Sprintf("tomorrow, %s", date.Format("January 2"))
+		when = "Tomorrow, " + date.Format("January 2")
 	} else {
-		when = fmt.Sprintf("%s", date.Format("Monday, January 2"))
+		when = date.Format("Monday, January 2")
 	}
 
 	switch day.FastLevel {
 	case 0:
-		text = fmt.Sprintf("%s, there is no fast.", when)
+		text = when + ", there is no fast."
 	case 1:
 		// normal weekly fast
 		if len(day.FastException) > 0 {
