8f4c5f39ee6544d57f8375495b7fc3986104aa0a
Brian Glass <bglass@safaribooksonline.com>
2018-04-09 17:48:08 -0600

    minor refactoring

--- alexa_test.go
+++ alexa_test.go
@@ -15,23 +15,19 @@ func TestEstimateGroupSize(t *testing.T) {
 	bible := orthocal.NewBible(db)
 
 	testCases := []struct {
-		reference  string
-		groupCount int
-		groupSize  int
+		reference string
+		groupSize int
 	}{
-		{"Matt 22.15-17", 1, -1},
-		{"Matt 22.15-23.39", 2, 36},
-		{"Wis 4, 6, 7, 2", 2, 50},
-		{"Matt 1:1-7:8", 3, 60},
+		{"Matt 22.15-17", -1},
+		{"Matt 22.15-23.39", 36},
+		{"Wis 4, 6, 7, 2", 50},
+		{"Matt 1:1-7:8", 60},
 	}
 
 	for _, tc := range testCases {
 		t.Run("Estimate Passage", func(t *testing.T) {
 			passage := bible.Lookup(tc.reference)
-			groupCount, groupSize := EstimateGroupSize(passage)
-			if groupCount != tc.groupCount {
-				t.Errorf("groupCount should be %d but is %d", tc.groupCount, groupCount)
-			}
+			groupSize := EstimateGroupSize(passage)
 			if groupSize != tc.groupSize {
 				t.Errorf("groupSize should be %d but is %d", tc.groupSize, groupSize)
 			}
